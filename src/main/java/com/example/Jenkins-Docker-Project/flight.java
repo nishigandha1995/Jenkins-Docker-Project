d5c9dba7ad1434f2d0bc7130baafade85b8bd9d2
nishigandha1995 <119771780+nishigandha1995@users.noreply.github.com>
2024-03-27 14:43:30 +0530

    Create flight.java

---
+++ src/main/java/com/example/Jenkins-Docker-Project/flight.java
@@ -0,0 +1,14 @@
+package com.example.TATA;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/myflight")
+    public String getData(){
+        return "Please book your fav Flight ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
